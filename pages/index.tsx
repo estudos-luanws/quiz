92ed561f5f8439bce0508b25e309cf274a8268da
luanws <luan.w.silveira@gmail.com>
2021-01-25 11:19:49 -0300

    componentes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,44 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import { Widget } from '../src/components/Widget'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+const BackgroundImage = styled.div`
+  background-image: url(${db.bg});
+  flex: 1;
+`
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
 `
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <BackgroundImage>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>The legend of zelda</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>fasdfasdf asdfasdfas dfsdfsdasfd</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Header>
+            <h1>Quiz</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>fasdfasdf asdfasdfas dfsdfsdasfd</p>
+          </Widget.Content>
+        </Widget>
+      </QuizContainer>
+    </BackgroundImage>
+  )
 }
