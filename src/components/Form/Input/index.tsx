867a91255be36ca5b0bbabc3f903cf619bd48f4a
luanws <luan.w.silveira@gmail.com>
2021-01-27 21:33:58 -0300

    Update index.tsx

--- src/components/Form/Input/index.tsx
+++ src/components/Form/Input/index.tsx
@@ -1,12 +1,31 @@
+import { InputHTMLAttributes } from 'react'
 import styled from 'styled-components'
 
-const Input = styled.input`
-  padding: 8px;
+const InputBase = styled.input`
+  padding: 16px;
   background-color: transparent;
   border: solid 1px gray;
   border-radius: 5px;
-  margin-bottom: 16px;
   color: white;
+  font-size: 12pt;
+  flex: 1;
 `
+
+const Container = styled.div`
+  display: flex;
+  margin-bottom: 16px;
+`
+
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+
+}
+
+const Input: React.FC<Props> = function (props) {
+  return (
+    <Container>
+      <InputBase {...props} />
+    </Container>
+  )
+}
 
 export default Input
