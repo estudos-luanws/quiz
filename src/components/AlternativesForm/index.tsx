000ac0ce23cc652abcd2d7a2aa117c41ebaadfc2
luanws <luan.w.silveira@gmail.com>
2021-01-28 18:38:44 -0300

    Create index.tsx

---
+++ src/components/AlternativesForm/index.tsx
@@ -0,0 +1,31 @@
+import { HTMLFactory } from 'react'
+import styled from 'styled-components'
+
+const AlternativesForm: HTMLFactory<HTMLDivElement> = styled.form`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+
+  label {
+    &[data-selected="true"] {
+      background-color: ${({ theme }) => theme.colors.primary};
+      
+      &[data-status="SUCCESS"] {
+        background-color: ${({ theme }) => theme.colors.success};
+      }
+
+      &[data-status="ERROR"] {
+        background-color: ${({ theme }) => theme.colors.wrong};
+      }
+    }
+    &:focus {
+      opacity: 1;
+    } 
+  }
+
+  button {
+    margin-top: 24px;
+  }
+`;
+
+export default AlternativesForm;
