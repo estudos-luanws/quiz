4e2861e1c4d9229eb7d67abf5a89e9278aa0c336
luanws <luan.w.silveira@gmail.com>
2021-01-27 21:33:57 -0300

    Update index.tsx

--- src/components/Form/Button/index.tsx
+++ src/components/Form/Button/index.tsx
@@ -4,7 +4,7 @@ const Button = styled.button`
   background-color: ${({ theme }) => theme.colors.primary};
   color: white;
   border-radius: 5px;
-  padding: 8px;
+  padding: 12px;
   border: none;
   font-weight: bold;
   text-transform: uppercase;
