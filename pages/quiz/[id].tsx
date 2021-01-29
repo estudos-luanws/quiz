478d536d1e2756af53a44a946252bb629ef0f203
luanws <luan.w.silveira@gmail.com>
2021-01-29 15:50:21 -0300

    Update [id].tsx

--- pages/quiz/[id].tsx
+++ pages/quiz/[id].tsx
@@ -1,7 +1,16 @@
+import { GetServerSideProps } from "next/types"
+
 export default function QuizPage() {
   return (
     <div>
       Quiz externo
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async function (context) {
+  console.log(context.query.id)
+  return {
+    props: {}
+  }
 }
