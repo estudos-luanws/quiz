f33016501d270dc48c1f7956a8b00d7d53fc9b1d
luanws <luan.w.silveira@gmail.com>
2021-03-24 23:15:43 -0300

    Update index.tsx

--- src/components/QuizBackground/index.tsx
+++ src/components/QuizBackground/index.tsx
@@ -1,39 +1,17 @@
 import styled from 'styled-components'
 
-const QuizBackground = styled.div`
+const QuizBackground = styled.img.attrs((props) => ({
+  src: props['data-background']
+}))`
   position: fixed;
   z-index: -1;
   width: 100vw;
   height: 100vh;
-  background-size: cover;
-  background-position: center;
+  object-fit: cover;
+  object-position: top;
   background-color: ${({ theme }) => theme.colors.mainBg};
-  background-image: url(${(props) => props['data-background']});
   @media screen and (max-width: 500px) {
-    background-image: none;
-    &:after {
-      content: "";
-      background-size: cover;
-      background-position: center;
-      background-image:
-        linear-gradient(
-          transparent, 
-          ${({ theme }) => theme.colors.mainBg}), 
-          url(${(props) => props['data-background']}
-        );
-      display: block;
-      width: 100%;
-      height: 210px;
-      position: absolute;
-      top: 0;
-      left: 0;
-      right: 0;
-      z-index: 1;
-    }
-    *:first-child {
-      position: relative;
-      z-index: 10;
-    }
+    object-fit: contain;
   }
 `
 
